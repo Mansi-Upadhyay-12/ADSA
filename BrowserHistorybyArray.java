0ccefcfb5c8623e3f3e23bdc6b2355a56eb51c5e
Mansi-Upadhyay-12 <153119421+Mansi-Upadhyay-12@users.noreply.github.com>
2025-09-29 08:41:13 +0530

    Create BrowserHistorybyArray.java

---
+++ BrowserHistorybyArray.java
@@ -0,0 +1,28 @@
+class BrowserHistorybyArray {
+    String[] history;
+    int current;
+    int last;
+
+    public BrowserHistory(String homepage) {
+        history = new String[5000]; 
+        history[0] = homepage;
+        current = 0;
+        last = 0;
+    }
+
+    public void visit(String url) {
+        current++;
+        history[current] = url;
+        last = current;
+    }
+
+    public String back(int steps) {
+        current = Math.max(0, current - steps);
+        return history[current];
+    }
+
+    public String forward(int steps) {
+        current = Math.min(last, current + steps);
+        return history[current];
+    }
+}
